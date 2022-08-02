0f45ed956cde6df3f71d0021b1411a7de3bbb710
Daniel Sommermann <dcsommer@fb.com>
2022-08-02 07:56:50 -0700

    Fixes for highlighting repro

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-rm *.o *.a
-cc bar.c
-cc foo.c
+rm -f *.o *.a
+clang -c bar.c
+clang -c foo.c
 ar rcs libbar.a bar.o
 ar rcs libfoo.a foo.o
-rustc --crate-type=staticlib test.rs -C link-arg=foobar -l static:+whole-archive=foo -l static:-whole-archive=bar -L.
+
+rustc -v test.rs --test \
+      -C debuginfo=2 \
+      -l static:+whole-archive=foo -l static:-whole-archive=bar -L. \
+      -C link-arg=-Wl,--nonsense  # Add nonsense to the link line so we can investigate it
+
+echo "!!!!"
+echo 'Note that both `libfoo.a` and `libbar.a` are wrapped with whole-archive in the link line, despite `-l static:-whole-archive=bar`'
+echo "!!!!"
